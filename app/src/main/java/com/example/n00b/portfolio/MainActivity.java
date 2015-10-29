d49518ac378e64a7cf9ce07dcaf67305956196c0
rhyd3n <yeoh.chris@gmail.com>
2015-10-29 19:46:49 +0800

    update on landscape

--- app/src/main/java/com/example/n00b/portfolio/MainActivity.java
+++ app/src/main/java/com/example/n00b/portfolio/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.n00b.portfolio;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void popMoviesApp(View view){
+    //to do code
+        Context context = getApplicationContext();
+        CharSequence text = "This will launch Popular Movies App";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
+
+    public void scoresApp(View view){
+        //to do code
+        Context context = getApplicationContext();
+        CharSequence text = "This will launch Scores App";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
+
+    public void libraryApp(View view){
+        //to do code
+        Context context = getApplicationContext();
+        CharSequence text = "This will launch library App";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
+
+    public void buildApp(View view){
+        //to do code
+        Context context = getApplicationContext();
+        CharSequence text = "This will launch builder App";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
+
+    public void baconReaderApp(View view){
+        //to do code
+        Context context = getApplicationContext();
+        CharSequence text = "This will launch Bacon Reader App";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
+
+    public void myAppToast(View view){
+        //this is a pop up dialog function
+        Context context = getApplicationContext();
+        CharSequence text = "This is Kriz's App";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
 }
+
+
